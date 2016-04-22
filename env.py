e01ca7795ba6bd7f2eec43358558d70ce96ff36d
nick.grange <nicolas.grange@retrievercommunications.com>
2016-04-22 16:24:47 +1000

    added sending sensor data to rs api

--- env.py
+++ env.py
@@ -1,5 +1,8 @@
 from sense_hat import SenseHat
+import requests
+
 sense = SenseHat()
+counter = 1
 
 while True:
     t = sense.get_temperature()
@@ -9,7 +12,10 @@ while True:
     t = round(t, 1)
     p = round(p, 1)
     h = round(h, 1)
+    data = { 'id' : counter, 'temp' : t, 'humidity' : h, 'pressure' : p}
+    r = requests.post("http://192.168.6.23:8080/serviceui/api/iot/sensor/data", json = data)
+    print(r.status_code)
+    msg = "Temp = %s, Pressure=%s, Humidity=%s" % (t,p,h)
 
-    msg = "Temperature = %s, Pressure=%s, Humidity=%s" % (t,p,h)
-
-    sense.show_message(msg, scroll_speed=0.05)
+    sense.show_message(msg)
+    counter += 1
